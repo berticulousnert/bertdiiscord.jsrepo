40be274c38e2464ba6e41d58828d8d1a2023c778
berticulousnert <Nathanielnelson645@gmail.com>
2021-11-09 07:04:40 -0500

    fixing

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "help",
   code: `
-$apiMessage[$channelId;;{author:::}{title:╭•Help}{description: **Main Command** \`\`$getServerVar[prefix]Help\`\` \n Gives the website where all bert commands are.
+$addButton[1;;{author:::}{title:╭•Help}{description: **Main Command** \`\`$getServerVar[prefix]Help\`\` \n Gives the website where all bert commands are.
 **Easter egg** \n If you are able to find one of berts hidden command you get premium for one month. 
 
 **Extra info** \n to get to the commands scroll down a bit and this is not the finish product this can change at any time.} {color:#8700ff}{footer:Enjoy!:$authorAvatar};{actionRow:All Commands,2,5,https\\://www.bert-bot.tk/}};$messageID:false;no] 
