1abb3ffb115bb6b034378301b9ef4b8dc59108fc
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2021-12-15 05:10:34 +0000

    changes

--- commands/adminOnly/purge.js
+++ commands/adminOnly/purge.js
@@ -2,7 +2,7 @@ module.exports = ({
 name: "purge",
 code: `
 $sendMessage[**$message[1] Messages deleted successfully**{delete:2s};no]
-$clear[$message[1];everyone;$channelID;no]
+$clear[$message[1];everyone]
 $wait[2s]
 $deletecommand
 $onlyIf[$isNumber[$replaceText[$replaceText[$checkCondition[$message==];true;100];false;$message]]==true;:x: Please provide a valid amount! {delete:5s}]
