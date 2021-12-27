3fab8bf70ace14cfb2109b2f099d9e5bdd41ba61
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2021-12-27 16:57:25 +0000

    fixed av

--- commands/uncaticorised/av.js
+++ commands/uncaticorised/av.js
@@ -1,4 +1,4 @@
 module.exports = {
   name: "av",
-  code: `$image[1;$authorAvatar] $onlyIf[$getGlobalUserVar[Blacklist;$authorID]==false;**⛔ You are blacklisted**] $onlyIf[$getUserVar[command;$commandName]!=disabled;Command disabled]`
+  code: `$image[1;$authorAvatar]`
 }
