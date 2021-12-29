69d378b2f6299e353a1482b0d23314fc9aea0be4
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2021-12-29 22:09:32 +0000

    ea

--- commands/help/help.js
+++ commands/help/help.js
@@ -8,8 +8,7 @@ $title[1;╭•Help] $description[1;**Main Command** \`\`$getServerVar[prefix]He
 **Easter egg** \n If you are able to find one of berts hidden command you get premium for one month. 
 
 **Extra info** \n to get to the commands scroll down a bit and this is not the finish product this can change at any time.] $color[1;#8700ff] $footer[1;Enjoy!;$authorAvatar]
-
-$onlyIf[$getUserVar[command;$commandName]!=disabled;Command disabled]`
+`
 
 }
 //nop
