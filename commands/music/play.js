61ead5ba13aa85d7cb6dc6489c7268b31f8b8e72
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-01-03 23:53:06 +0000

    n

--- commands/music/play.js
+++ commands/music/play.js
@@ -6,6 +6,6 @@ module.exports = {
  $log[$lavalinkExecute[isIdling]|$lavalinkExecute[isPlaying]|$lavalinkExecute[isPaused]]
  $let[a;$lavalinkExecute[addtrack;$get[key];1]]
  $let[key;$lavalinkExecute[search;$message]]
-  $onlyif[$voiceid[$clientid]==;Someone is listening to songs in another Voice Channel\nEither join their Voice Channel or use this command later.]
+
  `
 };
