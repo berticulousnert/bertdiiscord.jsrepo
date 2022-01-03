884c696e553ca83c4c637e03626706aea95f81de
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-01-03 23:50:22 +0000

    lol

--- commands/music/joinVC.js
+++ commands/music/joinVC.js
@@ -5,6 +5,5 @@ module.exports = ({
   Successfully joined <#$voiceid[$authorid]>
   $lavaLinkExecute[connect]
   $onlyif[$voiceid[$clientid]==;Someone is listening to songs in another Voice Channel\nEither join their Voice Channel or use this command later.]
-  $lavalinkExecute[connect]
   `
   })
--- commands/music/play.js
+++ commands/music/play.js
@@ -1,10 +1,11 @@
 module.exports = {
  name: "play",
- code: `
+ code: `$onlyIf[$voiceid[$authorid]!=; Please join a Voice Channel and use this command.]
  Added $lavalinkExecute[songinfo;title] to queue
  $let[a;$lavalinkExecute[$replaceText[$replaceText[$lavalinkExecute[isIdling];true;play];false;volume]]]
  $log[$lavalinkExecute[isIdling]|$lavalinkExecute[isPlaying]|$lavalinkExecute[isPaused]]
  $let[a;$lavalinkExecute[addtrack;$get[key];1]]
  $let[key;$lavalinkExecute[search;$message]]
+  $onlyif[$voiceid[$clientid]==;Someone is listening to songs in another Voice Channel\nEither join their Voice Channel or use this command later.]
  `
 };
