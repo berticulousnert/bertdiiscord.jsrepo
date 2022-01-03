305b317b5cea1d6fa2dd859d76384330ddd53c14
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-01-03 23:46:37 +0000

    fix

--- commands/music/joinVC.js
+++ commands/music/joinVC.js
@@ -5,7 +5,6 @@ module.exports = ({
   Successfully joined <#$voiceid[$authorid]>
   $lavaLinkExecute[connect]
   $onlyif[$voiceid[$clientid]==;Someone is listening to songs in another Voice Channel\nEither join their Voice Channel or use this command later.]
-  $onlyIf[$voiceid[$authorid]!=; Please join a Voice Channel and use this command.]
   $lavalinkExecute[connect]
   `
   })
