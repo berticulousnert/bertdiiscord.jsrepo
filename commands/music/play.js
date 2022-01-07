8f572d70c8ea1509d104ce93adfc0e9f7214504d
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-01-07 01:55:12 +0000

    e

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,6 @@
 module.exports = {
  name: "play",
- code: `$suppressErrors[Connection error] $onlyIf[$voiceid[$authorid]!=; Please join a Voice Channel and use this command.]
+ code: `$argsCheck[1;Please say a song i should play or make the bot join a channel] $suppressErrors[Connection error] $onlyIf[$voiceid[$authorid]!=; Please join a Voice Channel and use this command.]
  Added $lavalinkExecute[songinfo;title] to queue
  $let[a;$lavalinkExecute[$replaceText[$replaceText[$lavalinkExecute[isIdling];true;play];false;volume]]]
  $log[$lavalinkExecute[isIdling]|$lavalinkExecute[isPlaying]|$lavalinkExecute[isPaused]]
