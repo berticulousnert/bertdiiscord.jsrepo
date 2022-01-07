d2c3377264254251f14b9a844e821368d6b33f52
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-01-07 02:15:57 +0000

    a

--- commands/adminOnly/autojoinrole.js
+++ commands/adminOnly/autojoinrole.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: "",
   type: "join",
-  code: `$onlyif[$guildID==821998081691746304;] $onlyif[$getServerVa[autojoinrole]!=;] $giveRole[$guildid;$authorID;$getServerVar[autojoinrole]] $log[Gave $userTag[$authorID] a role role]
+  code: `$onlyif[$guildID==821998081691746304;] $onlyif[$getServerVar[autojoinrole]!=;] $giveRole[$guildid;$authorID;$getServerVar[autojoinrole]]
+  u got ur role
   `
 }
