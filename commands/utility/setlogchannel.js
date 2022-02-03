70f316738f69745aa6ba5ac8caad94694c3dd23e
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-02-03 14:14:51 +0000

    fixed?

--- commands/utility/setlogchannel.js
+++ commands/utility/setlogchannel.js
@@ -3,5 +3,5 @@ module.exports = {
   code: `$setServerVar[wbid;$splitText[1]] $setServerVar[wbtk;$splitText[2]]
   $textSplit[$createWebhook[$getServerVar[logchannel];Bert Logs;$userAvatar[$clientID]; ]; ]
   $setServerVar[logchannel;$findChannel[$message]]
-  Log channel has been set to <#$findChannel[$message]>  $onlyPerms[managechannel;You must have manage channel perms bozo] $onlyBotPerms[managechannel;Heyy- can i pwitty pwease get manage channel perms?] $onlyif[$getServerVar[logchannel]==;Sorry a log channel has already been set please use $getServerVar[prefix]reset-logchannel to reset the log channel.`
+  Log channel has been set to <#$findChannel[$mentioned[1]]>  $onlyPerms[managechannel;You must have manage channel perms bozo] $onlyBotPerms[managechannel;Heyy- can i pwitty pwease get manage channel perms?] $onlyif[$getServerVar[logchannel]==;Sorry a log channel has already been set please use $getServerVar[prefix]reset-logchannel to reset the log channel. $suppressErrors`
 }
