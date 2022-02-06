8e0d4a9e393d00f56d9e10fe759be06e00c308aa
Supreme Supreme <91563714+berticulousnert@users.noreply.github.com>
2022-02-06 00:00:27 +0000

    added change loggs and suggestion and fixed toxic command

---
+++ commands/help/changelogs.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "changelogs",
+  code: `$title[1;Change Logs]
+$description[1;+ .toggle-toxic - This commands finds bad words and delete them 
++ suggest - So you can suggest commands]`
+}
---
+++ commands/priv/suggest.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: "suggest",
+  code: `$dm[$botownerID] From $userTag[$authorID] suggestion: $message $cooldown[5s;Chill homie]`
+}
