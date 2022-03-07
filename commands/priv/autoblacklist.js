461a38e6ebf2df472759a0d4b09f13aaf1fce894
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2022-03-07 10:43:43 -0600

    fix?

--- commands/priv/autoblacklist.js
+++ commands/priv/autoblacklist.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "$alwaysExecute",
     if: "v4",
     code: `
-    $if[$sub[$creationDate[$messageId;ms];$splitText[2]]>2999]
+    $if[$sub[$creationDate[$messageId;ms];$splitText[2]]>1999]
     $blacklist[globalUser;$authorID]
     $setGlobalUserVar[msgs;$splitText[2]^$splitText[3]^$splitText[4]^$splitText[5]^$splitText[6]^$splitText[7]^$splitText[8]^$splitText[9]^$creationDate[$messageId;ms]]
     $textSplit[$getGlobalUserVar[msgs];^] 
