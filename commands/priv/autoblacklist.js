545c1494175050bdf46aeeb76da86355d598c96d
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2022-03-06 23:31:37 -0600

    Create autoblacklist.js

---
+++ commands/priv/autoblacklist.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "$alwaysExecute",
+    if: "v4",
+    code: `
+    $if[$sub[$creationDate[$messageId;ms];$splitText[2]]>2999]
+    $blacklist[globalUser;$authorID]
+    $setGlobalUserVar[msgs;$splitText[2]^$splitText[3]^$splitText[4]^$splitText[5]^$splitText[6]^$splitText[7]^$splitText[8]^$splitText[9]^$creationDate[$messageId;ms]]
+    $textSplit[$getGlobalUserVar[msgs];^] 
+    $onlyIf[$commandInfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
+    $onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]`
+  }
